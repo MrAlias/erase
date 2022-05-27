91687ec642acded016158d107f155270e084d335
Tyler Yahn <codingalias@gmail.com>
2022-05-27 12:34:09 -0700

    Add Tasty mushroom listing to mushrooms

--- mushrooms/mushroom.go
+++ mushrooms/mushroom.go
@@ -1,6 +1,17 @@
 // Package mushrooms provides mushrooms and information about them.
 package mushrooms
 
+var Tasty = []Mushroom{
+	{"agaricus bisporus", "button mushroom"},
+	{"boletus edulis", "porcini"},
+	{"cantharellus cibarius", "chanterelle"},
+	{"clitocybe nuda", "blewit"},
+	{"lactarius deliciosus", "saffron milk cap"},
+	{"morchella esculenta", "morel"},
+	{"pleurotus ostreatus", "oyster mushroom"},
+	{"tricholoma matsutake", "matsutake"},
+}
+
 var Poisonous = []Mushroom{
 	{"amanita muscaria", "fly agaric"},
 	{"amanita ocreata", "death angle"},
